5fbe82cf62f19b5047a355a9b39448860c13e27e
RodinPetr <rodinp@mail.ua>
2017-05-12 10:07:57 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="windows-1251">
+		<title>Главная</title>
+		<link href="style.css" type="text/css" rel="stylesheet">
+	</head>
+	<body>
+		<header>
+			<center><div id="welcome">
+				<span><img src="img/lt.png" width="40px"></span>
+				<span id="topname"><b>LemonStore.pp.ua</b></span>
+				<span><img src="img/ltr.png" width="60px"></span>
+			</div></center>
+			<div id="menue">
+				<center><div id="navi"><a href="index.php">Главная</a></div>
+				<div id="navi"><a href="shop.php">Товары</a></div>
+				<div id="navi"><a href="delivery.html">Доставка</a></div>
+				<div id="navi"><a href="pay.html">Оплата</a></div>
+				<div id="navi"><a href="contact.php">Обратная связь</a></div>
+				</center>
+			
+			</div>
+		</header>
+		<div id="crr">
+			<div id="ltt">
+				<img src="img/mn.png" id="pict">
+					Добро пожаловать! Предлагаем вам вступить в наши сообщества что бы вы могли следить за новостями и быть вкурсе акций, розыгрышей и разных бонусов.
+
+
+
+			</div>
+			<div id="rtt">
+				<img src="img/fr.png" id="pict">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum orci vitae eros malesuada tempus. Integer sit amet augue vestibulum, aliquet lectus eu, fringilla lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque viverra massa ligula, ac venenatis mauris auctor nec. Sed sed pretium ex, iaculis maximus diam. Quisque at efficitur metus. Sed augue libero, pellentesque vel turpis et, malesuada euismod nulla. Duis est eros, tempus feugiat arcu quis, rhoncus sagittis sem. Ut mattis aliquam maximus. Donec suscipit, tellus lacinia tincidunt cursus, turpis mauris volutpat libero, eu accumsan risus ante ut neque.
+
+				
+	
+
+			</div>
+			<div id="ltt">
+				<img src="img/mn.png" id="pict">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum orci vitae eros malesuada tempus. Integer sit amet augue vestibulum, aliquet lectus eu, fringilla lorem. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque viverra massa ligula, ac venenatis mauris auctor nec. Sed sed pretium ex, iaculis maximus diam. Quisque at efficitur metus. Sed augue libero, pellentesque vel turpis et, malesuada euismod nulla. Duis est eros, tempus feugiat arcu quis, rhoncus sagittis sem. Ut mattis aliquam maximus. Donec suscipit, tellus lacinia tincidunt cursus, turpis mauris volutpat libero, eu accumsan risus ante ut neque.
+
+
+
+			</div>
+			<hr>
+		</div>
+			
+		
+
+		
+	</body>
+</html>
